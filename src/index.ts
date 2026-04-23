8a512236c7d534dbbf7f241d64d218b2b244c774
SnowCait <SnowCait@users.noreply.github.com>
2026-04-24 01:25:34 +0900

    Timecapsule

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,29 @@
 import { Hono } from "hono";
+import { Nip11 } from "nostr-typedef";
+export { Relay } from "./relay";
 
 const app = new Hono<{ Bindings: CloudflareBindings }>();
 
-app.get("/message", (c) => {
-  return c.text("Hello Hono!");
+app.options("/", (c) => {
+  console.log("options");
+  c.header("Access-Control-Allow-Methods", "GET, OPTIONS");
+  return new Response(null, { status: 204 });
+});
+
+app.get("/", (c) => {
+  console.log("get");
+  if (c.req.header("Upgrade") === "websocket") {
+    const stub = c.env.RELAY.getByName("relay");
+    return stub.fetch(c.req.raw);
+  } else if (c.req.header("Accept") === "application/nostr+json") {
+    c.header("Access-Control-Allow-Origin", "*");
+    return c.json({
+      name: "Timecapsule",
+      software: "https://github.com/SnowCait/timecapsule",
+    } satisfies Nip11.RelayInfo);
+  } else {
+    return c.notFound();
+  }
 });
 
 export default app;
---
+++ src/relay.ts
@@ -0,0 +1,91 @@
+import { DurableObject, env } from "cloudflare:workers";
+import { Event } from "nostr-typedef";
+import { createRxNostr, now } from "rx-nostr";
+
+export class Relay extends DurableObject {
+  fetch(request: Request): Response {
+    console.log(request);
+    const { 0: client, 1: server } = new WebSocketPair();
+    this.ctx.acceptWebSocket(server);
+    return new Response(null, { status: 101, webSocket: client });
+  }
+
+  async webSocketMessage(
+    ws: WebSocket,
+    message: string | ArrayBuffer,
+  ): Promise<void> {
+    if (typeof message !== "string") {
+      return;
+    }
+
+    try {
+      const [type, ...args] = JSON.parse(message);
+      switch (type) {
+        case "EVENT": {
+          const event = args[0] as Event;
+          const publishAt = event.created_at;
+          const _now = now();
+          if (publishAt <= _now) {
+            ws.send(
+              JSON.stringify([
+                "OK",
+                event.id,
+                false,
+                "blocked: this relay accepts only future events",
+              ]),
+            );
+            break;
+          } else if (publishAt > _now + 24 * 60 * 60) {
+            ws.send(
+              JSON.stringify([
+                "OK",
+                event.id,
+                false,
+                "blocked: currently accepts events within 24 hours",
+              ]),
+            );
+            break;
+          }
+          await this.ctx.storage.put([event.id, publishAt].join(":"), event);
+          await this.ctx.storage.setAlarm(publishAt * 1000);
+          ws.send(JSON.stringify(["OK", event.id, true, ""]));
+          break;
+        }
+        default: {
+          ws.send(
+            JSON.stringify([
+              "NOTICE",
+              "unsupported: this relay supports only EVENT",
+            ]),
+          );
+        }
+      }
+    } catch {
+      ws.send(JSON.stringify(["NOTICE", "error: something wrong"]));
+    }
+  }
+
+  async alarm(): Promise<void> {
+    const events = await this.ctx.storage.list<Event>();
+    const rxNostr = createRxNostr();
+    rxNostr.setDefaultRelays(env.DEFAULT_RELAYS);
+    const { promise, resolve } = Promise.withResolvers<void>();
+    const _now = now();
+    for (const [key, event] of events) {
+      const [, publishAt] = key.split(":");
+      if (Number(publishAt) > _now) {
+        continue;
+      }
+      console.log(event);
+      await this.ctx.storage.delete(key);
+      rxNostr.send(event).subscribe({
+        complete: async () => {
+          await this.ctx.storage.delete(key);
+          resolve();
+        },
+      });
+    }
+    await promise;
+    rxNostr.dispose();
+  }
+}
